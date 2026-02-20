3bf33f9ad54839266a2f731789ad7c28407493c3
Da1sypetals <da1sypetals.iota@gmail.com>
2026-02-20 16:10:35 +0800

    Initial Commit

---
+++ ttsui-mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ttsui-mac
+//
+//  Created by daisy on 2026/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ttsui-mac/ttsui_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttsui_macApp.swift
+//  ttsui-mac
+//
+//  Created by daisy on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct ttsui_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
