84cc597b2cbf5e936f0a1d4044e78b1d73a4169e
Da1sypetals <da1sypetals.iota@gmail.com>
2026-02-23 15:56:54 +0800

    Fix window size

--- ttsui-mac/ttsui_macApp.swift
+++ ttsui-mac/ttsui_macApp.swift
@@ -26,10 +26,28 @@ struct ttsui_macApp: App {
                     // Shut down server when app exits
                     TTSServerManager.shared.stopServer()
                 }
+                .frame(width: 780, height: 975)
+                .background(WindowAccessor())
         }
         .windowStyle(.automatic)
+        .windowResizability(.contentSize)
         .commands {
             CommandGroup(replacing: .newItem) { }
         }
     }
+}
+
+struct WindowAccessor: NSViewRepresentable {
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async {
+            if let window = view.window {
+                window.styleMask.remove(.resizable)
+                window.setContentSize(NSSize(width: 780, height: 975))
+            }
+        }
+        return view
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {}
 }
